04f316a2c25d6e58bab750f96b9324c4a6925525
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-16 12:46:38 +1200

    options menu
    added options menu that can mute sounds in the game!

---
+++ scripts/menu_options.gd
@@ -0,0 +1,13 @@
+extends Control
+@onready var sfx_menu_click = $"sfx menu click"
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(0,value)
+	sfx_menu_click.play()
+
+func _on_volume_mute_toggled(toggled_on):
+	AudioServer.set_bus_mute(0,toggled_on)
+	
