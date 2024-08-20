29758aef970f08422033f4abb6350741fc1d1534
22218-Burnside <22218@burnside.school.nz>
2024-08-20 19:45:40 +1200

    Breakout - changind how paddle moves around

--- scripts/flipper.gd
+++ scripts/flipper.gd
@@ -4,9 +4,9 @@ const SPEED = 50
 
 #allows the player to move left and right
 func _process(delta):
-	if position.x > 10 and Input.is_action_pressed("flipper_left"):
+	if position.x > 125 and Input.is_action_pressed("flipper_left"):
 		#we can move left
 		position.x -= SPEED 
-	if position.x < 720 and Input.is_action_pressed("flipper_right"):
+	if position.x < 625 and Input.is_action_pressed("flipper_right"):
 		#we can move left
 		position.x += SPEED 
