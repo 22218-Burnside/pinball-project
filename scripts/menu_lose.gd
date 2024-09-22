94c367cafa656396e6dad42814c4e66819188496
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-23 10:50:54 +1200

    Moved bloxcks around

--- scripts/menu_lose.gd
+++ scripts/menu_lose.gd
@@ -15,3 +15,7 @@ func _on_menu_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_retry_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu_win.tscn")
